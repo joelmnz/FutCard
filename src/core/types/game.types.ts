5e6fdd22b1d591e2b5c2a73eb52bb9f7ed73177d
joel@jknet.dev <joel@jknet.dev>
2026-03-23 05:52:52 +0000

    chore: strip out non-working svelte app, back to basics

--- src/core/types/game.types.ts
+++
@@ -1,7 +0,0 @@
-export interface GameState {
-  managerName: string
-  coins: number
-  xpPool: number
-  onboardingDone: boolean
-  lastSaved: number
-}
--- src/core/types/market.types.ts
+++
@@ -1,9 +0,0 @@
-import type { Card } from './card.types'
-
-export interface MarketListing {
-  listingId: string
-  card: Card
-  listPrice: number
-  listedAt: number
-  isPlayerListed: boolean
-}
